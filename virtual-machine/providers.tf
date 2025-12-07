c1bd895c3e81cfc9071b8d5abe7e17789ded9630
Glenn Benz <glenn.benz@gmail.com>
2025-12-06 19:25:14 -0500

    Don't look up the subscription

--- virtual-machine/providers.tf
+++ virtual-machine/providers.tf
@@ -1,9 +1,9 @@
-data "azurerm_subscriptions" "all" {}
+#data "azurerm_subscriptions" "all" {}
 
 provider "azurerm" {
   features {}
-  subscription_id = one([
-    for s in data.azurerm_subscriptions.all.subscriptions : s.subscription_id
-    if s.display_name == var.subscription_name
-  ])
+#  subscription_id = one([
+#    for s in data.azurerm_subscriptions.all.subscriptions : s.subscription_id
+#    if s.display_name == var.subscription_name
+#  ])
 }
