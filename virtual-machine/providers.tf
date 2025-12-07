a86e3877861f1deebccca73c05498dac6f6c8e24
Glenn Benz <glenn.benz@gmail.com>
2025-12-06 19:41:09 -0500

    Hardcode the subscription id

--- virtual-machine/providers.tf
+++ virtual-machine/providers.tf
@@ -2,6 +2,7 @@
 
 provider "azurerm" {
   features {}
+  subscription_id = "81fb6f8e-e791-4ef5-ac89-8b3259d4b23e"
 #  subscription_id = one([
 #    for s in data.azurerm_subscriptions.all.subscriptions : s.subscription_id
 #    if s.display_name == var.subscription_name
